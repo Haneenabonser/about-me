a9772e06faf2758ba6051967cf0f8f748cdfe8a5
Haneenabonser <aabonser@gmail.com>
2021-03-30 23:33:52 +0300

    refactor the code lab03

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # LAB 02 
 
 ## About Me, with a Guessing Game  
-In this website I mentiond a biography about me and I created 5 yes or no questions so the user can get to know me as a website owner better.
+In this website I mentiond a biography about me and I created 5 yes or no questions so the user can get to know me as a website owner better. Then I extend the game by creating a gussing number game with 4 attempts and 6 attempts to guess something about me using an array. 
+
 
 
 **Author**: Haneen Abonser
@@ -9,4 +10,4 @@ In this website I mentiond a biography about me and I created 5 yes or no questi
 **Reflections and Comments**
 - overall, this is going fine so far.
 - I do not have any question about what we have learned actually I am wondering what we will learn and how the final product or website will be? I'm very exciting.
-- I thought this will take two days but it took me less than two hours to finish it. I don't know if that is a long time or not but I know that I'm happy and proud.  
+- I thought this will take days and it took me almost 5 hours to finish it.
--- js/app.js
+++ js/app.js
@@ -1,37 +1,43 @@
 'use strict';
 
 let userName = prompt('Whats\'s your name?');
-alert('Hello ' + userName + '\rWelcome to our website, let\'s have some fun!')
+while (!userName) {
+    userName = prompt('Please write somthing');
+}
 
+alert('Hello ' + userName + '\nWelcome to our website, let\'s have some fun!')
 
+let score = 0;
 
 let foodLover = prompt('Am I a food lover (foodie person)?');
 foodLover = foodLover.toLowerCase();
 
-if (foodLover == 'yes' || foodLover == 'y') {
+if (foodLover === 'yes' || foodLover === 'y') {
     // console.log('Great! Your answer is correct.');
     alert('Great! Your answer is correct.');
-} else if (foodLover == 'no' || foodLover == 'n') {
+    score++;
+} else if (foodLover === 'no' || foodLover === 'n') {
     // console.log('Your answer is not correct');
     alert('Your answer is not correct');
 } else {
     //  console.log('Please anwser with yes or no');
-     alert('Please anwser with yes or no');  
+    alert('Please anwser with yes/y or no/n');
 }
 
 
 let sistersBrothers = prompt('Do I have any sisters or brothers?');
 sistersBrothers = sistersBrothers.toLowerCase();
 
-if (sistersBrothers == 'yes' || sistersBrothers == 'y') {
+if (sistersBrothers === 'yes' || sistersBrothers === 'y') {
     // console.log('Great! Your answer is correct.');
     alert('Great! Your answer is correct.');
-} else if (sistersBrothers == 'no' || sistersBrothers == 'n') {
+    score++;
+} else if (sistersBrothers === 'no' || sistersBrothers === 'n') {
     // console.log('Your answer is not correct');
     alert('Your answer is not correct');
-} else { 
+} else {
     // console.log('Please anwser with yes or no'); 
-    alert('Please anwser with yes or no');  
+    alert('Please anwser with yes/y or no/n');
 
 }
 
@@ -40,34 +46,36 @@ let likeMusic = prompt('Do I like music?');
 likeMusic = likeMusic.toLowerCase();
 
 
-if (likeMusic == 'yes' || likeMusic == 'y') {
+if (likeMusic === 'yes' || likeMusic === 'y') {
     // console.log('Great! Your answer is correct.');
     alert('Great! Your answer is correct.');
-} else if (likeMusic == 'no' || likeMusic == 'n') {
+    score++;
+} else if (likeMusic === 'no' || likeMusic === 'n') {
     // console.log('Your answer is not correct');
     alert('Your answer is not correct');
 } else {
     //  console.log('Please anwser with yes or no');
-     alert('Please anwser with yes or no');  
+    alert('Please anwser with yes/y or no/n');
 
-     }
+}
 
 
 let watchFootball = prompt('Do I watch football matches?');
 watchFootball = watchFootball.toLowerCase();
 
 
-if (watchFootball == 'yes' || watchFootball == 'y') {
+if (watchFootball === 'yes' || watchFootball === 'y') {
     // console.log('Great! Your answer is correct.');
     alert('Great! Your answer is correct.');
-} else if (watchFootball == 'no' || watchFootball == 'n') {
+    score++;
+} else if (watchFootball === 'no' || watchFootball === 'n') {
     // console.log('Your answer is not correct');
     alert('Your answer is not correct');
 } else {
     //  console.log('Please anwser with yes or no'); 
-    alert('Please anwser with yes or no');  
+    alert('Please anwser with yes/y or no/n');
 
-    }
+}
 
 
 
@@ -75,17 +83,70 @@ let wearingColors = prompt('Do I like wearing colors?');
 wearingColors = wearingColors.toLowerCase();
 
 
-if (wearingColors == 'yes' || wearingColors == 'y') {
+if (wearingColors === 'yes' || wearingColors === 'y') {
     // console.log('Your answer is not correct');
     alert('Your answer is not correct');
-} else if (wearingColors == 'no' || wearingColors == 'n') {
+} else if (wearingColors === 'no' || wearingColors === 'n') {
     // console.log('Great! Your answer is correct.');
     alert('Great! Your answer is correct.');
+    score++;
 } else {
     //  console.log('Please anwser with yes or no'); 
-    alert('Please anwser with yes or no');  
+    alert('Please anwser with yes/y or no/n');
 
+}
+
+let number=5;
+let guessNumber = prompt('Guess a number between 0 and 10?');
+guessNumber = parseInt(guessNumber);
+for (let i = 0; i < 3; i++) {
+    if (guessNumber === number) {
+        alert('Great! you are correct');
+        score++;
+        break;
+    } else if (guessNumber < number) {
+        prompt('This is too low, try another number');
+    } else if (guessNumber > number) {
+        prompt('This is too high, try another number');
     }
+}
+alert('The correct number is 5');
+
+// let number=5;
+// let maxAttempts=4; 
+// let tries=0;
+// let guessNumber=prompt('Guess a number between 0 and 10?');
+// while (tries<=maxAttempts){ 
+//     if (guessNumber === number) {
+//         alert('Great! you are correct');
+//         score++;
+//         break;
+//     } else if (guessNumber < number) {
+//         prompt('This is too low, try another number');
+//         tries++;
+//     } else if (guessNumber > number) {
+//         prompt('This is too high, try another number');
+//         tries++
+//     }else if( tries > maxAttempts){
+//         alert('You have no more tries left.');
+//     }
 
 
-alert('We hope you enjoy the game ' + userName);
+
+
+let favoriteColor = prompt('What do you think my favorite color is?\n red\nblack\nwhite\npink\nblue\npurple');
+favoriteColor = favoriteColor.toLowerCase();
+let colors = ['blue', 'black', 'purple'];
+for (let i = 1; i < 6; i++) {
+    if (favoriteColor === colors[0] || favoriteColor === colors[1] || favoriteColor === colors[2]) {
+        alert('Great! your answer is correct');
+        score++;
+        break;
+    }
+    else {
+        prompt('Guess another color');
+    }
+}
+alert('You are run out of attempts!');
+
+alert('We hope you enjoy the game ' + userName + ', your score is ' + score);
